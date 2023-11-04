0bd0d3d0b47df98349af5dcbe2581e2ff4451977
Leonardo Amerio <leoamerio14@gmail.com>
2023-11-04 12:36:24 -0300

    stores & middlewares

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,4 +1,6 @@
-import { WhiteCard } from '../../components';
+import { useShallow } from 'zustand/react/shallow';
+import { WhiteCard } from "../../components";
+import { useBearStore } from "../../stores";
 
 export const BearPage = () => {
   return (
@@ -9,45 +11,88 @@ export const BearPage = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
 
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
+        <BlackBears />
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
+        <PandaBears />
 
-        </WhiteCard>
+        <PolarBears />
 
-        <WhiteCard centered>
-          <h2>Osos Polares</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-
-          </div>
-
-        </WhiteCard>
-
-        <WhiteCard centered>
-          <h2>Osos Pandas</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
-
-
-
-
+        <BearsDisplay />
       </div>
-
     </>
   );
 };
+
+export const BlackBears = () => {
+  const blackBears = useBearStore((state) => state.blackBears);
+  const increaseBlackBears = useBearStore((state) => state.increaseBlackBears);
+
+  return (
+    <WhiteCard centered>
+      <h2>Osos Negros</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increaseBlackBears(+1)}> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> {blackBears} </span>
+        <button onClick={() => increaseBlackBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
+
+export const PandaBears = () => {
+  const pandaBears = useBearStore((state) => state.pandaBears);
+  const increasePandaBears = useBearStore((state) => state.increasePandaBears);
+
+  return (
+    <WhiteCard centered>
+      <h2>Osos Panda</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increasePandaBears(+1)}> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> {pandaBears} </span>
+        <button onClick={() => increasePandaBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
+
+export const PolarBears = () => {
+  const polarBears = useBearStore((state) => state.polarBears);
+  const increasePolarBears = useBearStore((state) => state.increasePolarBears);
+  // const {polarBears, increasePolarBears} = useBearStore(state => state) //? se podria desestructurar? 
+  // Si, pero no es recomendado,
+  // ya que useBearStore estara pendiente de cada cambio de state, lo que genera que cada ves que aumente o decremente en 
+  // otro oso tambien va a haber un re-render de este pq cambio el estado, por mas que no se haya cambiado ESTE componente
+
+  return (
+    <WhiteCard centered>
+      <h2>Osos Polares</h2>
+
+      <div className="flex flex-col md:flex-row">
+        <button onClick={() => increasePolarBears(+1)}> +1</button>
+        <span className="text-3xl mx-2 lg:mx-10"> {polarBears} </span>
+        <button onClick={() => increasePolarBears(-1)}>-1</button>
+      </div>
+    </WhiteCard>
+  );
+};
+
+export const BearsDisplay = () => {
+  const bear = useBearStore(useShallow(state => state.bears))
+  const doNothing = useBearStore(state => state.Nothing);
+  const addBears = useBearStore(state => state.addBears);
+  const clearBears = useBearStore(state => state.clearBears);
+
+  return (
+    <WhiteCard>
+      <h1>Osos</h1>
+      <button onClick={doNothing}>Do Nothing</button>
+      <button className="mt-2" onClick={addBears}>Add</button>
+      <button className="mt-2" onClick={clearBears}>Clear</button>
+      <pre>
+        {JSON.stringify(bear, null, 2)}
+      </pre>
+    </WhiteCard>
+  )
+}
---
+++ src/stores/storages/firebase.storage.ts
@@ -0,0 +1,24 @@
+import { StateStorage, createJSONStorage } from 'zustand/middleware';
+
+const firebaseUrl = 'https://zustand-storage-leoamerio-default-rtdb.firebaseio.com/zustand';
+
+const firebaseAPI: StateStorage = {
+  getItem: async function ( name: string ): Promise<string | null> {
+      const data = await fetch(`${firebaseUrl}/${ name }.json`).then(res => res.json());
+      console.log(data)
+      return JSON.stringify( data );
+    
+  },
+  setItem: async function ( name: string, value: string ): Promise<void> {
+    const data = await fetch(`${firebaseUrl}/${ name }.json`, {
+      method: 'PUT',
+      body: value
+    }).then(res => res.json());
+    return;
+  },
+  removeItem: function ( name: string ): void | Promise<void> {
+    console.log('removeItem', name);
+  }
+}
+
+export const customFirebaseStorage = createJSONStorage(() => firebaseAPI)
